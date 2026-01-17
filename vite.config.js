7ea82162cf86e396ad1e4fee051ba374ed5b1e1a
Baby-Madara <ahmedfarahat1177@gmail.com>
2026-01-17 20:47:10 +0200

    last jan 2026

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 
 export default defineConfig({
-    plugins: [react()],
-    base: '/TechNinja/', // Replace with your repository name
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                src: 'src/images', // Path to the folder you want to include
+                dest: 'src/',    // Destination folder in the output (dist)
+                },
+            ],
+        }),
+    ],
+    base: '/TechNinja/',
 })
